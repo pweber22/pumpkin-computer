08110d396961b358d0b0ce52e2e46c7751061e21
pweber22 <pweber.mn@gmail.com>
2022-05-20 13:13:27 -0500

    created a new Target header file for privacy

---
+++ PumpkinComputer/TargetExample.h
@@ -0,0 +1,6 @@
+//Rename this file Target.h
+
+double targetLat = 41.917004; // target latitude in decimal degrees
+double targetLon = -91.443742;  // target longitude in decimal degrees
+int targetAlt_ft = 805;  // target altitude in feet MSL
+int bearing = 276; // bearing of the flight path over the target
